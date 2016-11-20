27bfbbf74227969706f56ef164711cce7710a2bd
hopegi <690383065@qq.com>
2016-11-20 15:54:38 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!............");
+            Console.ReadKey();
         }
     }
 }
